f28af4157b8bbdf7b4ec94d82ad9256594e0025d
mutistic-client <mutistic@qq.com>
2018-08-13 09:54:21 +0800

    双向适配器

--- com.mutistic.principle/src/com/mutistic/design/adapter/logger/Client.java
+++ com.mutistic.principle/src/com/mutistic/design/adapter/logger/Client.java
@@ -2,18 +2,33 @@ package com.mutistic.design.adapter.logger;
 
 import java.util.Date;
 
+import com.mutistic.common.utils.PrintUtil;
+
+/**
+ * @program 演示 双向适配器
+ * @description 
+ * @author mutisitic
+ * @date 2018年8月13日
+ */
 public class Client {
 
 	public static void main(String[] args) {
+		
+		PrintUtil.printOne("演示 双向适配器");
 		LoggerMode mode = new LoggerMode();
 		mode.setCreaterTime(new Date());
 		mode.setLeave("INFO");
 		mode.setMessage("演示双向适配器");
+		
 		TwoDirectAdapter adapter = new TwoDirectAdapter(new LogDataSource(), new LogFileOperation());
+		PrintUtil.println();
+		
+		// log file method
 		adapter.writeLogFile(mode);
 		adapter.readLogFile();
 		adapter.deleteLogFile(mode);
 		
+		// db method
 		adapter.createrLog(mode);
 		adapter.queryAll();
 		adapter.updateLog(mode);
--- com.mutistic.principle/src/com/mutistic/design/adapter/logger/TwoDirectAdapter.java
+++ com.mutistic.principle/src/com/mutistic/design/adapter/logger/TwoDirectAdapter.java
@@ -2,50 +2,74 @@ package com.mutistic.design.adapter.logger;
 
 import java.util.List;
 
+import com.mutistic.common.utils.PrintUtil;
+
+/**
+ * @program 双向适配器
+ * @description
+ * @author mutisitic
+ * @date 2018年8月13日
+ */
 public class TwoDirectAdapter implements LogDataSourceApi, LogFileOpearionApi {
 
 	private LogDataSourceApi logDataSourceApi;
 	private LogFileOpearionApi logFileOpearionApi;
-	
+
 	public TwoDirectAdapter(LogDataSourceApi logDataSourceApi, LogFileOpearionApi logFileOpearionApi) {
 		super();
 		this.logDataSourceApi = logDataSourceApi;
 		this.logFileOpearionApi = logFileOpearionApi;
+		
+		PrintUtil.printThree("双向适配器-TwoDirectAdapter()-构造函数：注入logDataSourceApi", logDataSourceApi.getClass());
+		PrintUtil.printThree("双向适配器-TwoDirectAdapter()-构造函数：注入logDataSourceApi", logFileOpearionApi.getClass());
 	}
 
 	@Override
 	public Integer writeLogFile(LoggerMode mode) {
 		logFileOpearionApi.writeLogFile(mode);
+		
+		PrintUtil.printThree("logDataSourceApi.writeLogFile()", mode.toString());
+
 		return null;
 	}
 
 	@Override
 	public Integer deleteLogFile(LoggerMode mode) {
 		logFileOpearionApi.deleteLogFile(mode);
+		
+		PrintUtil.printThree("logDataSourceApi.deleteLogFile()", mode.toString());
 		return null;
 	}
 
 	@Override
 	public List<LoggerMode> readLogFile() {
 		logFileOpearionApi.readLogFile();
+		
+		PrintUtil.printThree("logFileOpearionApi.readLogFile()", null);
 		return null;
 	}
 
 	@Override
 	public Integer createrLog(LoggerMode mode) {
 		logDataSourceApi.createrLog(mode);
+		
+		PrintUtil.printThree("logDataSourceApi.createrLog()", mode.toString());
 		return null;
 	}
 
 	@Override
 	public Integer updateLog(LoggerMode mode) {
 		logDataSourceApi.updateLog(mode);
+		
+		PrintUtil.printThree("logDataSourceApi.updateLog()", mode.toString());
 		return null;
 	}
 
 	@Override
 	public Integer removeLog(LoggerMode mode) {
 		logDataSourceApi.removeLog(mode);
+		
+		PrintUtil.printThree("logDataSourceApi.removeLog()", mode.toString());
 		return null;
 	}
 
@@ -54,5 +78,5 @@ public class TwoDirectAdapter implements LogDataSourceApi, LogFileOpearionApi {
 		logDataSourceApi.queryAll();
 		return null;
 	}
-	
+
 }
