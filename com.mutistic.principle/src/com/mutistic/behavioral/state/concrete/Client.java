709de1936008ed3adf242945f8bed9eb0ff697f4
mutistic-client <mutistic@qq.com>
2018-08-29 18:08:49 +0800

    在状态的处理类里面实现状态维护和状态转换的控制

--- com.mutistic.principle/src/com/mutistic/behavioral/state/concrete/Client.java
+++ com.mutistic.principle/src/com/mutistic/behavioral/state/concrete/Client.java
@@ -4,19 +4,13 @@ import com.mutistic.utils.PrintUtil;
 
 /**
  * @program Client：客户端
- * @description 演示 状态模式[State Pattern]
+ * @description 演示 在状态的处理类里面实现状态维护和状态转换的控制
  * @author mutisitic
  * @date 2018年8月29日
  */
 public class Client {
 	public static void main(String[] args) {
-		PrintUtil.one("状态模式[State Pattern]");
+		PrintUtil.one("在状态的处理类里面实现状态维护和状态转换的控制");
 		
-		Context ctx = new Context();
-		ctx.setState(new ConcreteStateA());
-		ctx.request("ConcreteStateA");
-		
-		ctx.setState(new ConcreteStateB());
-		ctx.request("ConcreteStateB");
 	}
 }
