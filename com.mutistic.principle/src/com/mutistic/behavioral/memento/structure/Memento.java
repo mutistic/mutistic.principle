b24a467639f764c1246845be4c7709f8a5c5767f
mutistic-client <mutistic@qq.com>
2018-08-31 09:52:44 +0800

    备忘录模式[Memento Pattern]

--- com.mutistic.principle/src/com/mutistic/behavioral/memento/structure/Memento.java
+++ com.mutistic.principle/src/com/mutistic/behavioral/memento/structure/Memento.java
@@ -4,6 +4,7 @@ package com.mutistic.behavioral.memento.structure;
  * @program Memento：备忘录。定义为窄接口（不声明任何方法）
  * @description 主要用来存储原发器对象的内部状态，但是具体需要存储哪些数据是由原发器对象来决定的。
  * 另外备忘录应该只能由原发器对象来访问它内部的数据，原发器外部的对象不应该能访问到备忘录对象的内部数据
+ * 一般定义为窄接口（不声明任何方法）,标识对象的类型
  * @author mutisitic
  * @date 2018年8月30日
  */
