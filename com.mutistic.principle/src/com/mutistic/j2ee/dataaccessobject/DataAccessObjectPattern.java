7c5d39abf2f93371b4912dc088652cf388060ce9
mutistic-client <mutistic@qq.com>
2018-09-17 10:26:03 +0800

    三十七、数据访问对象模式[Data Access Object Pattern]

--- com.mutistic.principle/src/com/mutistic/j2ee/dataaccessobject/DataAccessObjectPattern.java
+++ com.mutistic.principle/src/com/mutistic/j2ee/dataaccessobject/DataAccessObjectPattern.java
@@ -11,6 +11,19 @@ import com.mutistic.utils.PrintUtil;
 public class DataAccessObjectPattern {
 	public static void main(String[] args) {
 		PrintUtil.ln("数据访问对象模式[Data Access Object Pattern]");
-		PrintUtil.ln("");
+		PrintUtil.ln("一、定义、本质:\r\n" + 
+				"\r\n" + 
+				"定义: 把低级的数据访问 API 或操作从高级的业务服务中分离出来。\r\n" + 
+				"本质: 分离数据操作接口\r\n" + 
+				"\r\n" + 
+				"二、结构和说明:\r\n" + 
+				"\r\n" + 
+				"DataAccessObjectInterface：数据访问对象接口，该接口定义了在一个模型对象上要执行的标准操作。\r\n" + 
+				"\r\n" + 
+				"DataAccessObjectConcreteClass：数据访问对象实体类，该类实现了数据访问对象接口。该类负责从数据源获取数据，\r\n" + 
+				"数据源可以是数据库，也可以是 xml，或者是其他的存储机制。\r\n" + 
+				"\r\n" + 
+				"ModelObject/ValueObject：模型对象/数值对象，该对象是简单的POJO，包含了get/set方法来存储通过使用DAO类检索到的数据\r\n" + 
+				"");
 	}
 }
